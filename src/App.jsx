166f968efb4c61f1a0e8e7cd233aa07299cda699
Tj <tjeidanjack@gmail.com>
2025-01-13 01:51:04 +0000

    set up bare bones UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react"
+import { Header } from "./components/Header"
+import { InfoButton } from "./components/InfoButton"
+import { InfoPane } from "./components/InfoPane";
+import { Dealer } from "./components/Dealer";
+import { GameButton } from "./components/GameButton";
+import { Player } from "./components/Player";
+import { CardsHolder } from "./components/CardsHolder";
+
 function App() {
+  const [showInfo, setShowInfo] = useState(false);
   return (
-    <>
-    </>
+    <div id="content">
+      <Header />
+      <InfoButton setShowInfo={setShowInfo}/>
+      {showInfo ? <InfoPane /> : null}
+      <Dealer />
+      <CardsHolder />
+      <GameButton purpose={"Hit"}/>
+      <GameButton purpose={"Stand"}/>
+      <Player />
+      <CardsHolder />
+    </div>
   )
 }
 
