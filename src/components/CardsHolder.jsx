dafaf8ac860be23374b61c6987985823a5de8b8a
Tj <tjeidanjack@gmail.com>
2025-01-13 01:52:16 +0000

    create cardsholder component

---
+++ src/components/CardsHolder.jsx
@@ -0,0 +1,11 @@
+import { Card } from "./Card";
+
+export const CardsHolder = () => {
+    return (
+        <div>
+            <p>Cards go here</p>
+            <Card />
+            <Card />
+        </div>
+    )
+}
